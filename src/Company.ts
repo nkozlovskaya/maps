276fa1cf097d84eec29fd91b23a5b905810d0bc6
Nadezhda <catsnake@yandex.ru>
2021-09-09 12:59:27 +0300

    app show location of random user and company on googlemaps

---
+++ src/Company.ts
@@ -0,0 +1,33 @@
+import faker from 'faker';
+
+import { Mappable } from './CustomMap';
+
+export class Company implements Mappable{
+  companyName: string;
+  companyPhrase: string;
+  location: {
+    lat : number;
+    lng : number;
+  };
+
+  color : string = "black";
+
+  constructor() {
+    this.companyName = faker.company.companyName();
+    this.companyPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat : parseFloat(faker.address.latitude()),
+      lng : parseFloat(faker.address.longitude()),
+    }
+  }
+
+  markerContent(): string{
+    return `
+      <div>
+        <h1>Company Name: ${this.companyName}</h1>
+        <h2>Company CatchPhrase: ${this.companyPhrase}</h2>
+      </div>
+    `;
+  }
+}
+
